c0d3501bab094adb6e3ee4cc5c6789c7c40b1f58
Soohyun <csh0sns@naver.com>
2022-06-16 21:19:17 +0900

    폴더구조 정리 / route 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import {Route, Routes} from "react-router-dom";
+import {Home} from "./pages"
 
 function App() {
     return (
-        <div className="App">
-        </div>
+        <Routes>
+            <Route path="/*" element={<Home/>}/>
+        </Routes>
     );
 }
 
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../styles/Home/Home.css";
+
+const Home = () => {
+    return (
+        <div id="home">
+            여기는 메인페이지!
+        </div>
+    )
+}
+
+export default Home;
