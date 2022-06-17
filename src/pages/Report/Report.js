721ec8a625f8316fdf1f9cf16de2400330ef134d
hursuhyun <hursuhyun@gmail.com>
2022-06-17 18:33:33 +0900

    added input for report

--- src/pages/Report/Report.js
+++ src/pages/Report/Report.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { url } from "../../services/api";
+import { Button, TextField, Typography } from "@mui/material";
+import axios from "axios";
 
 const Report = () => {
-    return <div id="report">리포트!</div>;
+  const name = "";
+
+  const handleSubmit = () => {};
+  const getAnimal = (name) => {
+    axios.get(url, { params: { animal_name: name } });
+  };
+
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setinitState({
+      ...initState,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div id="report">
+      <form className="form" onSubmit={handleSubmit}>
+        <TextField
+          label="name of animal"
+          id="name"
+          name="name"
+          placeholder="choco"
+          value={name}
+          variant="outlined"
+          type="text"
+          onChange
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Report;
