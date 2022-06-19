a2c899e296d509f95bc5a0161ff5aeec2468e9e5
hursuhyun <hursuhyun@gmail.com>
2022-06-19 14:49:46 +0900

    modify backend api

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,17 +2,22 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
     app.use(
-        "/api/report",
+        "/api",
         createProxyMiddleware({
             target: "http://52.78.82.160:8080",
             changeOrigin: true,
+            onProxyRes: (proxyRes, req, res) => {
+                // log original request and proxied request info
+                const exchange = `[${req.method}] [${proxyRes.statusCode}] ${req.path} -> ${proxyRes.req.protocol}//${proxyRes.req.host}${proxyRes.req.path}`;
+                console.log(exchange); // [GET] [200] / -> http://www.example.com
+            },
         })
     );
-    app.use(
-        "/openApi",
-        createProxyMiddleware({
-            target: "https://www.data.go.kr/data/15098931/openapi.do",
-            changeOrigin: true,
-        })
-    );
+    // app.use(
+    //     "/openApi",
+    //     createProxyMiddleware({
+    //         target: "https://www.data.go.kr/data/15098931/openapi.do",
+    //         changeOrigin: true,
+    //     })
+    // );
 };
