2e43eaff3ec3cd299f0e8dea8e2055b70217471d
Soohyun <csh0sns@naver.com>
2022-06-20 21:28:28 +0900

    Footer 수정

--- src/pages/Common/Footer.js
+++ src/pages/Common/Footer.js
@@ -1,9 +1,13 @@
 import React from "react";
+import "../../styles/Common/Footer.css";
 
 const Footer = () => {
     return (
         <div id="footer">
-            푸터!
+            <div className="footer-contents-box">
+                <div>2022년 봄학기 벤처창업 웹 프로그래밍2</div>
+                <div>Final Project | Team 3</div>
+            </div>
         </div>
     )
 }
