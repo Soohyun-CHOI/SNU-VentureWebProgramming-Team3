0f20049807fc6a1fd6403c51412eaf048d87e7e6
Soohyun <csh0sns@naver.com>
2022-06-20 21:12:06 +0900

    Header 수정

--- src/pages/Common/Header.js
+++ src/pages/Common/Header.js
@@ -1,9 +1,19 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
+import "../../styles/Common/Header.css";
 
 const Header = () => {
     return (
         <div id="header">
-            헤더!
+            <div className="header-contents-box">
+                <NavLink className="left-side" to="/">사이트이름</NavLink>
+                <div className="nav">
+                    <NavLink className="nav-item" to="/animals">유기유실동물 공고</NavLink>
+                    <NavLink className="nav-item" to="/report">분실신고</NavLink>
+                    <NavLink className="nav-item" to="/mbti">동물티아이</NavLink>
+                </div>
+                <div className="right-side">로그인</div>
+            </div>
         </div>
     )
 }
