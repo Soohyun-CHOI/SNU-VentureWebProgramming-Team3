bc0cecfddccf1060f8b3e8db8f3c416ccd172b0a
Soohyun <csh0sns@naver.com>
2022-06-21 02:53:26 +0900

    LostList 수정 / Banner 분리

---
+++ src/pages/LostAnimalList/LostAnimalItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const LostAnimalItem = (props) => {
+    return (
+        <div id="lost-animal-item">
+            <img src={props.lostAnimal.image_url} alt=""/>
+            <div className="right-side">
+                <div className="traits">
+                    <div className="traits-title">등록 일자</div>
+                    <div>{props.lostAnimal.created_at.substr(0, 10)}</div>
+                </div>
+                <div className="traits">
+                    <div className="traits-title">특징</div>
+                    <div>{props.lostAnimal.feature}</div>
+                </div>
+                <div className="traits">
+                    <div className="traits-title">품종</div>
+                    <div>{props.lostAnimal.kind}</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LostAnimalItem;
--- src/pages/LostAnimalList/LostAnimalList.js
+++ src/pages/LostAnimalList/LostAnimalList.js
@@ -1,42 +1,44 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import toast from "react-hot-toast";
+import LostAnimalItem from "./LostAnimalItem";
+import Banner from "../Common/Banner";
+import {NavLink} from "react-router-dom";
 
 const LostAnimalList = () => {
-    const [lostList, setlostList] = useState([]);
-    console.log(lostList);
+    const [lostList, setLostList] = useState([]);
+
+    useEffect(() => {
+        getList();
+    }, []);
 
     const getList = (num = 5) => {
-        axios
-            .get("/api/animals/v2", { params: { num: num } })
-            .then((res) => {
-                setlostList(res.data.animals);
+        axios.get("/api/animals/v2", {params: {num: num}})
+            .then(res => {
+                setLostList(res.data.animals);
             })
             .catch((err) => {
-                toast.error(
-                    "등록에 실패하였습니다." + err.response.data.detail[0].type
-                );
+                toast.error("등록에 실패하였습니다." + err.response.data.detail[0].type);
             });
     };
 
-    useEffect(getList, []);
-
     return (
-        <div>
-            {lostList?.map((lostAnimal) => (
-                <div key={lostAnimal.created_at}>
-                    <img
-                        src={lostAnimal.image_url}
-                        alt=""
-                        height={"200px"}
-                        width={"200px"}
-                    />
-                    <div>
-                        <div>{lostAnimal.created_at}</div>
-                        <div>{lostAnimal.feature}</div>
-                        <div>{lostAnimal.kind}</div>
-                    </div>
-                </div>
+        <div id="lost-animal-list">
+            <Banner title="분실신고"/>
+            <div className="summary">
+                ✔️ 여기는 동물을 분실한 경우 동물정보를 올려 분실 동물을 찾을 수 있는 공간입니다.<br/>
+                ✔️ 동물을 분실한 경우 동물보호법 제12조제1항 및 같은 법 시행규칙 제8조제1항 및 제9조제2항에 따라 등록대상동물을
+                10일 이내 시장, 군수, 구청장에게 분실 신고하셔야 합니다.
+            </div>
+            <div className="top-side">
+                <div className="list-num">총 {lostList.length} 건</div>
+                <NavLink to="/report">신고 등록하기</NavLink>
+            </div>
+            {lostList?.map(lostAnimal => (
+                <LostAnimalItem
+                    key={lostAnimal.id}
+                    lostAnimal={lostAnimal}
+                />
             ))}
         </div>
     );
