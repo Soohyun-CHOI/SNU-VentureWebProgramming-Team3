c63f1688a84074ab0c6f5a8d1dbdfd48ce0ad06b
piljaechae <piljae@rapportlabs.kr>
2022-06-21 21:57:57 +0900

    Add main files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
